5dbac35d295e8d9ac0f23bfda51dbc7c29526196
acponton <pon22008@byui.edu>
2023-01-18 20:38:08 -0600

    errors fixed

--- Assignment_Portal/scripts/time.js
+++ Assignment_Portal/scripts/time.js
@@ -1,2 +1,8 @@
 
-alert(document.lastModified)
+const currentYear = new Date();
+let year = currentYear.getFullYear();
+document.querySelector("#year").textContent = year;
+
+
+let dateModified = new Date(document.lastModified);
+document.querySelector("#updated-time").textContent = dateModified;
